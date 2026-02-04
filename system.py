7731a2e79e05a906084756c208e6b7460be690b1
Sharvesh <sl101@kent.ac.uk>
2026-02-04 19:33:11 +0000

    fix 7

--- system.py
+++ system.py
@@ -38,6 +38,7 @@ def run_system_monolith():
             
            
             n.append(new_name)
+            r.append(new_rank)
             print("Crew member added.")
             
         elif opt == "3":
