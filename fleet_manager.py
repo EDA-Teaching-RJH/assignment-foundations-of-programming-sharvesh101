3acee35f37125f6756344b65630677250691d91b
Sharvesh <sl101@kent.ac.uk>
2026-02-08 17:48:13 +0000

    display_menu() created

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,8 +5,13 @@ def init_database():
     IDs = ["1","2"]
     return Names , Ranks, Divisions,IDs
 
+def display_menu():
+    Names , Ranks , Divisons, IDs = init_database()
+    print(f"{'NAME':<10} | {'Ranks':<10} | {'Divisions':<10} | {'IDs':<10}")
+    for name ,rank , div, id in zip(Names,Ranks,Divisons,IDs):
+        print(f"{name:<11} {rank:<12} {div:<12} {id:<10}")
 
 def main():
-    pass
+    display_menu()
 
 main()
