e5c644a584da398c38ce9d1746c44b572d3ba916
Sharvesh <sl101@kent.ac.uk>
2026-02-08 17:45:45 +0000

    init_database() created

---
+++ fleet_manager.py
@@ -0,0 +1,12 @@
+def init_database():
+    Names = ["Jack", "Mike"]
+    Ranks = ["senior", "junior"]
+    Divisions = ["Dev","Ops"]
+    IDs = ["1","2"]
+    return Names , Ranks, Divisions,IDs
+
+
+def main():
+    pass
+
+main()
